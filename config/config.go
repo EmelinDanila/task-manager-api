002f1ba2e7591e990cc96088b61f427ae347b25c
EmelinDanila <d.emelin.qa@gmail.com>
2025-02-01 12:52:48 +0300

    Add config package with database connection handling
    - Added Database interface and RealDB implementation.
    - Added functions to load environment variables from a .env file.
    - Implemented database connection logic using GORM and PostgreSQL.

---
+++ config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Database interface for working with the database
+type Database interface {
+	Close() error
+	GetDB() *gorm.DB
+}
+
+// RealDB implementation of the Database interface for a real database
+type RealDB struct {
+	db *gorm.DB
+}
+
+// Close closes the connection to the database
+func (r *RealDB) Close() error {
+	sqlDB, err := r.db.DB()
+	if err != nil {
+		return err
+	}
+	return sqlDB.Close()
+}
+
+// GetDB returns the *gorm.DB instance
+func (r *RealDB) GetDB() *gorm.DB {
+	return r.db
+}
+
+// LoadEnvVars loads environment variables from the .env file
+func LoadEnvVars() {
+	err := godotenv.Load("d:\\task-manager-api\\.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+// ConnectDatabase connects to the database and returns a Database interface
+func ConnectDatabase() (Database, error) {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	return &RealDB{db: db}, nil
+}
