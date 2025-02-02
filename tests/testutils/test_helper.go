95f82e296f600e9e6a1c710f851fa69abd031c48
EmelinDanila <d.emelin.qa@gmail.com>
2025-02-02 22:29:52 +0300

    Add test database setup and teardown utilities
    - Implemented SetupTestDB to initialize the test database and migrate the Task model.
    - Implemented TeardownTestDB to clean up the test database by dropping the Task table and closing the database connection.

---
+++ tests/testutils/test_helper.go
@@ -0,0 +1,41 @@
+package testutils
+
+import (
+	"log"
+	"testing"
+
+	"github.com/EmelinDanila/task-manager-api/config"
+	"github.com/EmelinDanila/task-manager-api/models"
+)
+
+// SetupTestDB initializes the test database
+func SetupTestDB(t *testing.T) config.Database {
+	// Load environment variables
+	config.LoadEnvVars()
+
+	// Connect to the database
+	db, err := config.ConnectDatabase()
+	if err != nil {
+		t.Fatalf("Could not connect to the database: %v", err)
+	}
+
+	// Migrate the Task model
+	err = db.GetDB().AutoMigrate(&models.Task{})
+	if err != nil {
+		t.Fatalf("Could not migrate database: %v", err)
+	}
+
+	return db
+}
+
+// TeardownTestDB cleans up the test database
+func TeardownTestDB(db config.Database) {
+	// Drop the Task table after tests
+	db.GetDB().Migrator().DropTable(&models.Task{})
+
+	// Close the database connection
+	err := db.Close()
+	if err != nil {
+		log.Printf("Error closing database connection: %v", err)
+	}
+}
